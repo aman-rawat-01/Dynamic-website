bd2784be9eb38fe0c613aad98ba964331690b68b
Aman <amanrawat0919@gmail.com>
2023-04-12 11:22:25 +0530

    v2

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const hbs = require("hbs");
@@ -31,57 +32,62 @@ hbs.registerPartials("views/partials");
 mongoose.set("strictQuery", false);
 
 //connecting to mongo db
-mongoose.connect("mongodb://127.0.0.1:27017/websiteDB", () => {
-  console.log("DB connected");
+const user = process.env.DB_USER;
+const password = process.env.DB_PWD;
+mongoose.connect(
+  `mongodb+srv://${user}:${password}@cluster0.jqx9xwl.mongodb.net/websiteDB?retryWrites=true&w=majority`,
+  (err) => {
+    if (!err) console.log("DB connected");
+    else console.log("error:" + err);
+    //   Detail.create({
+    //     brandName: "Kaze",
+    //     brandIconUrl:
+    //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm4Rfprwyine2a8SK437EZBwixVhCJGpJ6mA&usqp=CAU",
+    //     links: [
+    //       {
+    //         label: "Home",
+    //         url: "/",
+    //       },
+    //       {
+    //         label: "Services",
+    //         url: "/services",
+    //       },
+    //       {
+    //         label: "Gallery",
+    //         url: "/gallery",
+    //       },
+    //       {
+    //         label: "About",
+    //         url: "/about",
+    //       },
+    //       {
+    //         label: "Contact Us",
+    //         url: "/contact-us",
+    //       },
+    //     ],
+    //   });
 
-  // Detail.create({
-  //   brandName: "Kaze",
-  //   brandIconUrl:
-  //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm4Rfprwyine2a8SK437EZBwixVhCJGpJ6mA&usqp=CAU",
-  //   links: [
-  //     {
-  //       label: "Home",
-  //       url: "/",
-  //     },
-  //     {
-  //       label: "Services",
-  //       url: "/services",
-  //     },
-  //     {
-  //       label: "Gallery",
-  //       url: "/gallery",
-  //     },
-  //     {
-  //       label: "About",
-  //       url: "/about",
-  //     },
-  //     {
-  //       label: "Contact Us",
-  //       url: "/contact-us",
-  //     },
-  //   ],
-  // });
+    //   Slider.create([
+    //     {
+    //       title: "Mt. Fuji",
+    //       description: "Fuji san sugoi desune.",
+    //       imageUrl:
+    //         "https://www.tripsavvy.com/thmb/qFqPcg6Wo24Hu4fLokNfAZdC-xQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/fuji-mountain-in-autumn-822273028-5a6a8a9c3418c600363958d3.jpg",
+    //     },
+    //   ]);
 
-  // Slider.create([
-  //   {
-  //     title: "Mt. Fuji",
-  //     description: "Fuji san sugoi desune.",
-  //     imageUrl:
-  //       "https://www.tripsavvy.com/thmb/qFqPcg6Wo24Hu4fLokNfAZdC-xQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/fuji-mountain-in-autumn-822273028-5a6a8a9c3418c600363958d3.jpg",
-  //   },....
-  // ]);
-
-  // Service.create([
-  //   {
-  //     icon: "fa-brands fa-accusoft",
-  //     title: "Best Japanese courses",
-  //     description:
-  //       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas officia dicta tenetur necessitatibus praesentium sint",
-  //     linkLabel: "Check",
-  //     linkUrl: "https://www.japanesepod101.com/",
-  //   },...
-  // ]);
-});
+    //   Service.create([
+    //     {
+    //       icon: "fa-brands fa-accusoft",
+    //       title: "Best Japanese courses",
+    //       description:
+    //         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas officia dicta tenetur necessitatibus praesentium sint",
+    //       linkLabel: "Check",
+    //       linkUrl: "https://www.japanesepod101.com/",
+    //     },
+    //   ]);
+  }
+);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running on port 8000");
